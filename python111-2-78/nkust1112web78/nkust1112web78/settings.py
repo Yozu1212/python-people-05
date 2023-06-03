92d90bdbd86d44108a7886b9f431224e28d691fa
Your Name <you@example.com>
2023-06-03 16:09:33 +0800

    message

--- python111-2-78/nkust1112web78/nkust1112web78/settings.py
+++ python111-2-78/nkust1112web78/nkust1112web78/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+STATIC_URL = '/static/'
